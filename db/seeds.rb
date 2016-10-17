3231b8f36ca2ce6b813f20679fd32904ae7bc3a6
Carlos Gomez <carlos221519@gmail.com>
2016-10-17 20:14:34 -0300

    init putasya.cl

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,4 @@
 
 AdminUser.destroy_all
 
-AdminUser.create!(email: 'admin@seuc.com', password: 'adminluis', password_confirmation: 'adminluis')
+AdminUser.create!(email: 'admin@putasya.com', password: 'adminadmin', password_confirmation: 'adminadmin')
